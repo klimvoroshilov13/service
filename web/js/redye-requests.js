af03eda82250d4364b07270df1b7e703b15aa28f
Kazakov_N <klimvoroshilov13@gmail.com>
2018-08-31 06:13:34 +0300

    Сегодняшные заявки выделяются шрифтом bold

--- web/js/redye-requests.js
+++ web/js/redye-requests.js
@@ -1,42 +1,44 @@
 'use strict';
 
 // Функция изменения цвета заявки взависимости от статуса
-var changeColor = function () {
+var redyeRequests = function () {
 
     var request = document.querySelectorAll('tr[data-key]');
-    // var regexDate = date;
+    var regexDate = formatDate(today);
     var regexPostponed = /отложена/;
     var regexWait = /ожидание/;
     var regexPerformed = /выполняется/;
+    var regexCompleted = /завершена/;
 
     for (var i = 0; i < request.length; i++) {
     var text = request[i].innerText;
     text.search(regexPostponed) > 0 ? request[i].className='postponed':null;
     text.search(regexWait) > 0 ? request[i].className='wait':null;
     text.search(regexPerformed) > 0 ? request[i].className='performed':null;
-    // text.search(regexDate) > 0 ? request[i].className='today':null;
+    text.search(regexDate) > 0 ? request[i].classList.add('today'):null;
+    text.search(regexCompleted) > 0 ? request[i].classList.remove('today'):null;
     }
 };
 
-// function formatDate(date) {
-//
-//     var dd = date.getDate();
-//     if (dd < 10) dd = '0' + dd;
-//
-//     var mm = date.getMonth() + 1;
-//     if (mm < 10) mm = '0' + mm;
-//
-//     var yyyy = date.getFullYear();
-//     // if (yy < 10) yy = '0' + yy;
-//
-//     return dd + '.' + mm + '.' + yyyy;
-// }
-//
-// var today = new Date;
+function formatDate(date) {
 
-$(document).ready(changeColor);
-$(document).on('pjax:complete',changeColor);
+    var dd = date.getDate();
+    if (dd < 10) dd = '0' + dd;
+
+    var mm = date.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+
+    var yyyy = date.getFullYear();
+    // if (yy < 10) yy = '0' + yy;
+
+    return dd + '.' + mm + '.' + yyyy;
+}
+
+var today = new Date;
+
+$(document).ready(redyeRequests);
+$(document).on('pjax:complete',redyeRequests);
 
 //Диагностика
 // var request = document.querySelectorAll('tr[data-key]');
-// console.log(formatDate(today));
+// console.log(request);

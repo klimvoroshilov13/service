4ebbfad3f6e5e8305e9c42ebb98ee8a684c7c276
klimvoroshilov13 <klimvoroshilov13@gmail.com>
2020-08-06 07:21:36 +0300

    Added new files with code text

---
+++ db/service_loc_apps.sql
@@ -0,0 +1,3 @@
+INSERT INTO `service.loc`.apps (id, name, url, section) VALUES (1, 'Заявки', 'requests', 'service');
+INSERT INTO `service.loc`.apps (id, name, url, section) VALUES (2, 'Планировщик', 'planner', 'service');
+INSERT INTO `service.loc`.apps (id, name, url, section) VALUES (3, 'Сервис', 'service', 'home');
---
+++ db/service_loc_sections.sql
@@ -0,0 +1,2 @@
+UPDATE `service.loc`.sections SET name = 'home' WHERE id = 1;
+UPDATE `service.loc`.sections SET name = 'service' WHERE id = 2;
